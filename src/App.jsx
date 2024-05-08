552a9f92ef6db64910890280e20f6c693f58860f
MituBaby <ucihazhaske@gmail.com>
2024-05-08 21:53:01 +0700

    feture login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
+import { LoginPage } from "./pages/LoginPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <HomePage />,
+    },
+    {
+        path: "/login",
+        element: <LoginPage />,
     },
 ]);
 
