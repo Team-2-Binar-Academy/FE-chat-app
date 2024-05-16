98a9bead1e39cad42576a19a28eb35d4a0c921de
Dwi875 <dwix2504@gmail.com>
2024-05-16 23:48:45 +0700

    adding register_page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      primary: "#933393",
+      secondary: "#FFF4FF",
     },
-    plugins: [],
+  },
+  plugins: [],
 };
