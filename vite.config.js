e30ec4963416d122367ddb9df76611bc580b9475
Ferry Febriansyah <perriebenhard@gmail.com>
2024-05-17 01:28:17 +0700

    fixing env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    define: {
+        "process.env.VITE_VERCEL_BACKEND_API": process.env.VITE_VERCEL_BACKEND_API,
+        "process.env.VITE_VERCEL_GOOGLE_CLIENT_ID": process.env.VITE_VERCEL_GOOGLE_CLIENT_ID,
+    },
+});
